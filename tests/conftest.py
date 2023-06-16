c6dfada496ef93affa0e2286cdf14dd17db22a32
Alex Stefan <cenumesapunn@gmail.com>
2023-06-16 23:29:21 +0300

    update, done with framework

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-
+driver = None
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -15,14 +15,15 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="class")
 def setup(request):
+    global driver
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
         chrome_options = Options()
         # chrome_options.add_experimental_option("detach", True)
         # linux
-        service_obj = Service("/usr/bin/chromedriver")
+        # service_obj = Service("/usr/bin/chromedriver")
         # work
-        # service_obj = Service("D:\ChromeDriver")
+        service_obj = Service("D:\ChromeDriver")
         driver = webdriver.Chrome(service=service_obj, options=chrome_options)
     elif browser_name == "firefox":
         service_obj = Service("/usr/bin/geckodriver")
@@ -31,11 +32,38 @@ def setup(request):
         #TODO IE invocation
         pass
 
+    driver.implicitly_wait(5)
     driver.get("https://rahulshettyacademy.com/angularpractice/")
     driver.maximize_window()
     request.cls.driver = driver
     yield
     driver.close()
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+def _capture_screenshot(name):
+        driver.get_screenshot_as_file(name)
 
 
 
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -6,7 +6,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from pageObjects.CheckoutPage import CheckOutPage
 from pageObjects.HomePage import HomePage
 from utilities.BaseClass import BaseClass
 
@@ -14,26 +13,30 @@ from utilities.BaseClass import BaseClass
 class TestOne(BaseClass):
 
     def test_e2e(self):
+        log = self.getLogger()
         homePage = HomePage(self.driver)
-        homePage.shopItems().click()
-        checkOutPage = CheckOutPage(self.driver)
+        checkOutPage = homePage.shopItems()
+        log.info("getting all the card titles")
         cards = checkOutPage.getCardTitles()
         i = -1
-        for card in cards:
-            i = i + 1
+        for index, card in enumerate(cards):
             cardText = card.text
+            log.info(cardText)
             if cardText == "Blackberry":
-                checkOutPage.getCardFooter()[i].click()
+                checkOutPage.getCardFooter()[index].click()
 
+        # TODO POM and optimize objects
         self.driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
-        checkOutPage.checkOutItems().click()
+        confirmPage = checkOutPage.checkOutItems()
+        log.info("Entering country name as ind")
         self.driver.find_element(By.ID, "country").send_keys("ind")
-        element = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.LINK_TEXT, "India")))
+        self.verifyLinkPresence(10, "India")
+
         # TODO POM
         self.driver.find_element(By.LINK_TEXT, "India").click()
         self.driver.find_element(By.XPATH, "//label[@for='checkbox2']").click()
         self.driver.find_element(By.XPATH, "//input[@class='btn btn-success btn-lg']").click()
 
         success_test = self.driver.find_element(By.CLASS_NAME, "alert-success").text
+        log.info(f"Text received from application is {success_test}")
         assert "Success! Thank you" in success_test
